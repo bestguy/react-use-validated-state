7ea86c4e8ce8c67428af8485e573bab351c1fd21
bestguy <gary@gary.local>
2019-11-28 20:47:02 -0800

    0.2.2

--- dist/validatejs.js
+++ dist/validatejs.js
@@ -3,23 +3,23 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
 
 var _index = _interopRequireDefault(require("./index"));
 
 var _validate = _interopRequireDefault(require("validate.js"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(defaultValue) {
   var constraints = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
   var validateImmediately = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
 
   var isInvalid = function isInvalid(value) {
-    return _validate.default.single(value, constraints);
+    return _validate["default"].single(value, constraints);
   };
 
-  return (0, _index.default)(defaultValue, isInvalid, validateImmediately);
+  return (0, _index["default"])(defaultValue, isInvalid, validateImmediately);
 };
 
-exports.default = _default;
+exports["default"] = _default;
